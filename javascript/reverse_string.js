225826936f7867b5de9e3b63c02df19501e59e3f
Brett de Bear <bkdebear@gmail.com>
2023-05-10 11:35:08 -0700

    reverseString function using for loop

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
-  // type your code here
+  let revString = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    revString += str[i];
+  }
+  return revString;
 }
 
 if (require.main === module) {
@@ -17,3 +22,13 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// Normally, I would use a built-in function that uses the split, reverse, and join methods chained together
+// For this, I use a for loop.
+
+//In line 2, I declare a variable called revString and set it equal to an empty string.
+// Line 4 is the start of the for loop. The loop initializes at the index of the last character in the string (let i = str.length - 1)
+    // Then, I set the condition to i >= 0, which meaans this will iterate as long as i is >= 0.
+    // Next, the loop decrements with each iteration. (i--)
+// In line 5, I'm appending each character in the string to revString in reverse order.
+// Lastly, in line 7, I am returning the revString variable.
